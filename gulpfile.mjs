a08d4cd20e253eadb8695e61eed7599260b067e5
Carlos Augusto Prado <carlos.prado@nerdweb.com.br>
2025-08-11 17:07:11 -0300

    Cria header do projeto

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -6,17 +6,19 @@ import browserSyncLib from "browser-sync";
 import concat from "gulp-concat";
 import babel from "gulp-babel";
 import uglify from "gulp-uglify";
-import cleanCSS from "gulp-clean-css"; // Importa o gulp-clean-css
+import cleanCSS from "gulp-clean-css";
 
-// Configuração para usar Dart Sass
-const sassCompiler = gulpSass(sass); // Configura o compilador corretamente
+const sassCompiler = gulpSass(sass);
 const browserSync = browserSyncLib.create();
 
-// Compilando o sass, adicionando autoprefixed e dando refresh na página
 function compilaSass() {
 	return gulp
-		.src("scss/*.scss")
-		.pipe(sassCompiler()) // Corrigido para usar o compilador configurado
+		.src("src/scss/**/*.scss")
+		.pipe(
+			sassCompiler({
+				includePaths: ["node_modules"],
+			}).on("error", sassCompiler.logError)
+		)
 		.pipe(
 			autoprefixer({
 				overrideBrowserslist: ["last 2 versions"],
@@ -24,14 +26,10 @@ function compilaSass() {
 			})
 		)
 
-		.pipe(concat("style.css"))
-		.pipe(gulp.dest("../"))
-		.pipe(gulp.dest("./"))
-
 		.pipe(cleanCSS({ level: 2 }))
-		.pipe(concat("style.min.css")) // Renomeia o arquivo minificado
-		.pipe(gulp.dest("../")) // Salva a versão minificada
-		.pipe(gulp.dest("./"))
+		.pipe(concat("style.min.css"))
+		.pipe(gulp.dest("src/css/"))
+		.pipe(gulp.dest("dist/css/"))
 		.pipe(browserSync.stream());
 }
 
@@ -39,13 +37,13 @@ function compilaSass() {
 export const sassTask = compilaSass;
 
 function pluginsCSS() {
-	return gulp.src("css/lib/*.css").pipe(concat("plugins.css")).pipe(gulp.dest("css/")).pipe(browserSync.stream());
+	return gulp.src("src/css/lib/*.css").pipe(concat("plugins.css")).pipe(gulp.dest("dist/css/")).pipe(gulp.dest("src/css/")).pipe(browserSync.stream());
 }
 export const plugincss = pluginsCSS;
 
 function gulpJs() {
 	return gulp
-		.src("js/scripts/*.js")
+		.src("src/js/scripts/*.js")
 		.pipe(concat("all.js"))
 		.pipe(
 			babel({
@@ -53,14 +51,14 @@ function gulpJs() {
 			})
 		)
 		.pipe(uglify())
-		.pipe(gulp.dest("js/"))
-		.pipe(gulp.dest("../js/"))
+		.pipe(gulp.dest("src/js/"))
+		.pipe(gulp.dest("dist/js/"))
 		.pipe(browserSync.stream());
 }
 export const alljs = gulpJs;
 
 function pluginsJs() {
-	return gulp.src("js/lib/*.js").pipe(concat("plugins.js")).pipe(gulp.dest("js/")).pipe(browserSync.stream());
+	return gulp.src("src/js/lib/*.js").pipe(concat("plugins.js")).pipe(gulp.dest("dist/js/")).pipe(gulp.dest("src/js/")).pipe(browserSync.stream());
 }
 export const pluginjs = pluginsJs;
 
@@ -68,7 +66,7 @@ export const pluginjs = pluginsJs;
 function browser() {
 	browserSync.init({
 		server: {
-			baseDir: "./",
+			baseDir: "./src/",
 		},
 	});
 }
@@ -76,11 +74,11 @@ export const browserSyncTask = browser;
 
 // Função do watch para alterações em SCSS e HTML
 function watchFiles() {
-	gulp.watch("scss/*.scss", compilaSass);
-	gulp.watch("css/lib/*.css", pluginsCSS);
-	gulp.watch("*.html").on("change", browserSync.reload);
-	gulp.watch("js/scripts/*.js", gulpJs);
-	gulp.watch("js/lib/*.js", pluginsJs);
+	gulp.watch("src/scss/**/*.scss", compilaSass);
+	gulp.watch("src/css/lib/*.css", pluginsCSS);
+	gulp.watch("src/*.html").on("change", browserSync.reload);
+	gulp.watch("src/js/scripts/*.js", gulpJs);
+	gulp.watch("src/js/lib/*.js", pluginsJs);
 }
 export const watch = watchFiles;
 
